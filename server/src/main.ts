ee3ed9cfca75aa6e61ae52b400dd8e5238b3fb97
MartinsDiego17 <martinsdiego0513@outlook.es>
2026-02-06 16:57:16 -0300

    ignore server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,34 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import serverless from 'serverless-http';
-import express from 'express';
 
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-let cachedHandler: any; // 👈 clave
-
-async function bootstrap(): Promise<any> {
-  if (!cachedHandler) {
-    const expressApp = express();
-    const adapter = new ExpressAdapter(expressApp);
-
-    const app = await NestFactory.create(AppModule, adapter);
-    app.setGlobalPrefix('api');
-    app.enableCors({
-      origin: true,
-      credentials: true,
-    });
-
-    await app.init();
-
-    cachedHandler = serverless(expressApp);
-  }
-
-  return cachedHandler;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
-
-export const handler = async (req: VercelRequest, res: VercelResponse) => {
-  const handler = await bootstrap();
-  return handler(req, res);
-};
+bootstrap();
