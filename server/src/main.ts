4177d8a1a5153cbf7f5db3d64882bc5b3443ed71
MartinsDiego17 <martinsdiego0513@outlook.es>
2026-03-06 12:53:45 -0300

    add vercel url to cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpErrorFilter());
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:3000', "http://zenda-production.com"],
+    origin: ['http://localhost:3000', "https://zenda-i3jv.vercel.app/"],
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
   });
