24eab7b6671b52c1d8210bf420d0f8f6d01436c8
MartinsDiego17 <martinsdiego0513@outlook.es>
2026-03-06 12:18:12 -0300

    modify next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/server/**', '**/node_modules/**'],
+    };
+    return config;
+  },
 };
-
 export default nextConfig;
