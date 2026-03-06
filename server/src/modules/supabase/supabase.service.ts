3befdb0078f32fb980b2e49ae1f62d4478fe2e6c
MartinsDiego17 <martinsdiego0513@outlook.es>
2026-03-06 13:37:39 -0300

    test supabase

--- server/src/modules/supabase/supabase.service.ts
+++ server/src/modules/supabase/supabase.service.ts
@@ -8,8 +8,10 @@ export class SupabaseService {
 
     constructor(private configService: ConfigService) {
         this.client = createClient(
-            this.configService.get<string>('supabase_url') || "",
-            this.configService.get<string>('supabase_api_key') || "",
+            /*             this.configService.get<string>('supabase_url') || "",
+                        this.configService.get<string>('supabase_api_key') || "", */
+            this.configService.get<string>('https://mgzkcybntwhtnsewnoeb.supabase.co') || "",
+            this.configService.get<string>('sb_publishable_lseuzWbPdww6sKitOZsQNA_CaGRvbd-') || "",
         );
     }
 
